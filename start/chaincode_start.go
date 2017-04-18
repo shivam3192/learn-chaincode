9d323b7d8dfb29297ce5cc636797e800a7b0438d
shivam3192 <shivamagarwal31081992@gmail.com>
2017-04-18 13:10:52 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -1,74 +1,145 @@
-/*
-Copyright IBM Corp 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
 	"errors"
 	"fmt"
 
+	"encoding/json"
+
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-// SimpleChaincode example simple Chaincode implementation
-type SimpleChaincode struct {
+
+type StudentChaincode struct {
 }
 
-// ============================================================================================================================
-// Main
-// ============================================================================================================================
-func main() {
-	err := shim.Start(new(SimpleChaincode))
+//custom data models
+
+
+type StudentApplication struct {
+	ID                     string        `json:"id"`
+	Name 				   string        `json:"name"`
+	Marks   		       int           `json:"marks"`
+	}
+
+func GetStudentInfo(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Printf("Entering GetStudentInfo")
+
+	if len(args) < 1 {
+		fmt.Printf("Invalid number of arguments")
+		return nil, errors.New("Missing student application ID")
+	}
+
+	var studentApplicationId = args[0]
+	bytes, err := stub.GetState(studentApplicationId)
 	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("Could not fetch student application with id "+studentApplicationId+" from ledger", err)
+		return nil, err
 	}
+	return bytes, nil
 }
 
-// Init resets all the things
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+func CreateStudentApplication(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Printf("Entering CreatestudentApplication")
+
+	if len(args) < 2 {
+		fmt.Printf("Invalid number of args")
+		return nil, errors.New("Expected atleast two arguments for student application creation")
 	}
 
+	var studentApplicationId = args[0]
+	var studentApplicationInput = args[1]
+
+	err := stub.PutState(studentApplicationId, []byte(studentApplicationInput))
+	if err != nil {
+		("Could not save student application to ledger", err)
+		return nil, err
+	}
+
+	
+	fmt.Printf("Successfully saved student application")
+	return nil, nil
+
+}
+
+/**
+Updates the status of the student application
+**/
+/*
+func UpdatestudentApplication(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Printf("Entering UpdatestudentApplication")
+
+	if len(args) < 2 {
+		fmt.Printf("Invalid number of args")
+		return nil, errors.New("Expected atleast two arguments for student application update")
+	}
+
+	var studentApplicationId = args[0]
+	var status = args[1]
+
+	laBytes, err := stub.GetState(studentApplicationId)
+	if err != nil {
+		fmt.Printf("Could not fetch student application from ledger", err)
+		return nil, err
+	}
+	var studentApplication StudentApplication
+	err = json.Unmarshal(laBytes, &studentApplication)
+	studentApplication.Status = status
+
+	laBytes, err = json.Marshal(&studentApplication)
+	if err != nil {
+		fmt.Printf("Could not marshal student application post update", err)
+		return nil, err
+	}
+
+	err = stub.PutState(studentApplicationId, laBytes)
+	if err != nil {
+		fmt.Printf("Could not save student application post update", err)
+		return nil, err
+	}
+
+	
+	fmt.Printf("Successfully updated student application")
+	return nil, nil
+
+}
+*/
+
+
+func (t *StudentChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	
 	return nil, nil
 }
 
-// Invoke is our entry point to invoke a chaincode function
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke is running " + function)
-
-	// Handle different functions
-	if function == "init" {													//initialize the chaincode state, used as reset
-		return t.Init(stub, "init", args)
+func (t *StudentChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	if function == "GetStudentInfo" {
+		return GetStudentInfo(stub, args)
 	}
-	fmt.Println("invoke did not find func: " + function)					//error
-
-	return nil, errors.New("Received unknown function invocation: " + function)
+	return nil, nil
 }
 
-// Query is our entry point for queries
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("query is running " + function)
 
-	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
+
+func (t *StudentChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	if function == "CreateStudentApplication" {
+		return CreateStudentApplication(stub, args)
+		} /*else {
+			if(function == "init")
+			return nil, errors.New(username + " does not have access to create a student application")
+		}
+*/
 	}
-	fmt.Println("query did not find func: " + function)						//error
+	return nil, nil
+}
 
-	return nil, errors.New("Received unknown function query: " + function)
+
+func main() {
+
+	err := shim.Start(new(StudentChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	} else {
+		fmt.Printf"StudentChaincode successfully started")
+	}
+
 }
